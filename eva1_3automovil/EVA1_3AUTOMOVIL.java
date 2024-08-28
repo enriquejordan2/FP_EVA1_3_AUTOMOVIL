c08b455165ec04e3401e89e4b4dea118ed1dcb4e
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-08-28 16:10:48 -0600

    Add files via upload

---
+++ eva1_3automovil/EVA1_3AUTOMOVIL.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3automovil;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA1_3AUTOMOVIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //DECLARACION DE LAS VARIABLES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+        System.out.println(marca);//imprime lo que esta definido en la variable marca y demas variables
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+        
+        
+    }
+    
+}
